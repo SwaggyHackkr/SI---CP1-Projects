53b3a1e0a763db32c32f7ca5f1aeeba5aa8c99ad
SwaggerHackkr <salesi.ilaoa@ucas-edu.net>
2026-08-18 09:11:34 -0600

    first commit

---
+++ inputsOutputs.py
@@ -0,0 +1,15 @@
+# comments
+
+print("Good morning!") # This is a output
+print("This is programming 1")
+print("As usual we are experiencing tehcnical difficulties!")
+
+# input
+
+name = input("What is your name: ")
+
+print("Welcome to class " + name)
+
+print("What did you do this weekend: ")
+weekend = input()
+print("Thats super cool i wish i played " + weekend + " this weekend.")
