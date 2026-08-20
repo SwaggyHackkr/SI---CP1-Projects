069ec5b195496c84d58b6c1f736c5899be4d8a6f
SwaggerHackkr <salesi.ilaoa@ucas-edu.net>
2026-08-20 09:06:05 -0600

    I changed the question input variable to weekend

---
+++ variables.py
@@ -0,0 +1,18 @@
+# Salesi Ilaoa, Variables Notes
+
+# Create a variable
+name = "salesi"
+age = 10
+gpa = 4.9
+
+print(f"Hello {name}")
+print(f"You're age is {age}")
+print(f"Your gpa is {gpa}")
+
+#reset variable 
+name = "Alex"
+
+print("Sorry I meant "+ name)
+
+# another variable example
+age = 10
