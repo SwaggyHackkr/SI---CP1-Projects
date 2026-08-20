aeb0f93616203edd597d6d2383a4e4758db2ec51
SwaggerHackkr <salesi.ilaoa@ucas-edu.net>
2026-08-20 09:44:44 -0600

    This is the assingment Character Intro!

---
+++ characterintro.py
@@ -0,0 +1,20 @@
+# Salesi Ilaoa, Character Introduction
+
+john_name = "john"
+age = 25
+job = "Teacher"
+# the scope of the variabes is shown above.
+
+print(f"Hi {john_name}!")
+
+print(f"your age is {age}")
+
+print(f"your job is a {job}!")
+
+question = input("Do you think your job is useful or not:")
+
+decide = input("Decide whether you would quit your job for 1 Million dollars or keep your stable job:")
+
+print("Wise decision to pick/choose " + decide)
+
+# the rest of the code above is a print statement, and a input and output example! 
