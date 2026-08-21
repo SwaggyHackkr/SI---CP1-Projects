2db26a936166d84cd9f4b212679fac7db11e1855
SwaggerHackkr <salesi.ilaoa@ucas-edu.net>
2026-08-21 09:51:57 -0600

    Debugging notes!

---
+++ debugging_notes.py
@@ -0,0 +1,24 @@
+# Salesi Ilaoa, Debugging Notes
+
+# Syntax Error
+
+#indentation error
+ #  <indentation error
+
+
+
+# Logic errors
+
+
+# logic errors are when you did something wrong with a step.
+
+# run time errors are happen when they are running sometimes 
+while: True
+    try:
+     fav_num = int(input("What is your favorite number: "))
+except:
+       print("Thats not a number!")
+else:
+    break
+
+ print(4 + fav_num)
