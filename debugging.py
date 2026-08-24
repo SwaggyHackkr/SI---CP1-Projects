1c3abac357943072bd47d917b25b9b2a2883e594
SwaggerHackkr <salesi.ilaoa@ucas-edu.net>
2026-08-24 09:13:10 -0600

    I added another bracket on the print statement and fixed the name variable in the print statement as well

---
+++ debugging.py
@@ -0,0 +1,6 @@
+name = "Alex"
+age = 16
+
+print("My name is " + name + " and I am " + str(age) + " years old.")
+#I added a ending bracket because there was only 1 bracket
+#I also changed the variable name from "nam" to "name" in the print code
