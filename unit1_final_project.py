0a53645fe9d290f9f2112d78e927833592e88840
SwaggerHackkr <salesi.ilaoa@ucas-edu.net>
2026-08-26 09:46:09 -0600

    Unit 1 final project, I have wrote my pseudocode for the first part of my final project explaining the steps of the code and explaining what the code will do.

--- unit1_final_project.py
+++ unit1_final_project.py
@@ -6,17 +6,40 @@
 
 # Variables I will be using for these questions are listed below.
 
-# name, age, hobby, hometown.
+# name, age, hobby, hometown, job
 
 
 #I will also be using inputs to grab the information out of them.
 
         #inputs 
-    #name = input("What is your name: ")
-    #age = input("what is your age: ")
-    #hobby = input("What is your favorite hobby: ")
-    #hometown = input("Where is your hometown: ")        
+    #name = input()
+    #age = input()
+    #hobby = input()
+    #hometown = input()     
     
 
     #This printed introduction will ask the inputs above and
      # will gather information.
+# the final printed statement will say 
+# Wow so your name is "name" and you are "age" your job is being a "job" your favorite hobby is "hobby" and your hometown is "hometown"
+# the quotations are the variable names that are inputs so when they type for example their name it will input the "name" of what they typed.
+
+print("Hello, and welcome to the information survey.")
+
+print("what is your name: ")
+name = input()
+print("that is a cool name " + name )
+
+print("how old are you: ")
+age = input()
+
+print("what is your job: ")
+job = input()
+
+print("what is your favorite hobby: ")
+hobby = input()
+
+print("where is your hometown: ")
+hometown = input()
+
+print("Wow so your name is " + name + " and you are " + age +" your job is being a " + job + " your favorite hobby is " + hobby + " and your hometown is " + hometown )
