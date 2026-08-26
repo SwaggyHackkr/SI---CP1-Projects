6366675acadc8a3df7e01b6c648497799a234ce5
SwaggerHackkr <salesi.ilaoa@ucas-edu.net>
2026-08-26 09:17:11 -0600

    Unit 1 final project, This is the pseudocode of the first part of my final project.

---
+++ unit1_final_project.py
@@ -0,0 +1,22 @@
+# Salesi Ilaoa, Unit 1 final project
+
+#Questions to ask
+# --What is your name, age, favorite hobby, and hometown?
+
+
+# Variables I will be using for these questions are listed below.
+
+# name, age, hobby, hometown.
+
+
+#I well also be using inputs to grab the information out of them.
+
+        #inputs 
+    #name = input("What is your name: ")
+    #age = input("what is your age: ")
+    #hobby = input("What is your favorite hobby: ")
+    #hometown = input("Where is your hometown: ")        
+    
+
+    #This printed introduction will ask the inputs above and
+     # will gather information.
