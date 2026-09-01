071ebfb3bb40f0245727692070efb86c9422439c
SwaggyHackkr <salesi.ilaoa@ucas-edu.net>
2026-09-01 09:19:26 -0600

    Integers and floats notes

---
+++ integers_floats.py
@@ -0,0 +1,30 @@
+#integers - A whole number 
+#Floats - Number with a decimal point. Just write the number with no extra syntax.
+
+#Arithmetic Operators - ( + - / // ** %)
+
+#Variables = 
+pi = 3.145
+
+#Modulo/Mod = % (modulus)
+print(5%2)
+print(10%4)
+print(15%5)
+
+#modulo gives us the remainder of a division problem
+
+num +=2 
+
+print(f"after {num}")
+
+print(f"After Mod {num}")
+
+#Expression 
+
+#Converting between data types
+
+man = float(input("what is your favorite number: "))
+
+print(f"{man**2} is {man} squared")
+print(int(pi))
+print(round(num,6))
