4ffb3b1f556f5c5ceaaacd3381b904901ae8b1ab
SwaggyHackkr <salesi.ilaoa@ucas-edu.net>
2026-09-01 09:52:38 -0600

    average grade assingment adding true or false statements.

---
+++ average_grade.py
@@ -0,0 +1,20 @@
+# Salesi Ilaoa, period: 1,  Average Grade assignment
+
+grade = float(input("Grade for English: "))
+grade_two = float(input("Grade for Math: "))
+grade_three = float(input("Grade for Geography: "))
+grade_four = float(input("Grade for Pre-med: "))
+grade_five = float(input("Grade for World civilization: "))
+grade_six = float(input("Grade for Biology: "))
+grade_seven = float(input("Grade for Health: "))
+
+#Finding the average grade
+
+
+while: True
+    try: 
+        grade = float(input("Grade for English: "))
+    except:
+        print("that is not a number!")
+    else:
+        break
