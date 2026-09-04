d542c92758e976bee3305ae59aa9173b4bc1f6fc
only1zazzy <salesiilaoa@gmail.com>
2026-09-03 18:31:11 -0600

    Salesi Ilaoa, Period 1, Madlib Assignment. This is my madlib assignment.

--- madlib.py
+++ madlib.py
@@ -9,7 +9,7 @@ verb1 = input("Enter a verb: ")
 place = input("Enter a place: ")
 
 # Build the story using string concatenation
-story = "Once upon a time, there was a " + adjective1 + " " + noun1 + " who loved to " + verb1 + " in " + place + ". " " The " + noun1 + " would " + verb1 + " every single day while eating a giant " + noun2 + ". " "One day, the " +adjective1 + " " + noun1 + " discovered a golden " + noun2 + " in " + place + ". " " Everyone in " + place + " cheered for the " + adjective1 + " " + noun2 + " and the golden " + noun2 + ". "
+story = "Once upon a time, there was a " + adjective1 + " " + noun1 + " who loved to " + verb1 + " in " + place + ". " " The " + noun1 + " would " + verb1 + " every single day while eating a giant " + noun2 + ". " "One day, the " +adjective1 + " " + noun1 + " discovered a golden " + noun2 + " in " + place + ". " " Everyone in " + place + " cheered for the " + adjective1 + " " + noun1 + " and the golden " + noun2 + ". "
 
 #Print the story
 print(story)
