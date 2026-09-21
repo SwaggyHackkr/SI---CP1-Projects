2cf8e0af82972a01d39496b135210f4b47485b58
SwaggyHackkr <salesi.ilaoa@ucas-edu.net>
2026-09-21 09:06:52 -0600

    I have done my pseudocode for the crew shares assignment

---
+++ crew_shares.py
@@ -0,0 +1,24 @@
+#Salesi Ilaoa, Period 1, Crew shares assignment.
+
+#Pseudocode of the crew shares 
+
+#GET number of pirates including Yondu and Quill
+#GET random number between 500 and 5000
+#SET total units TO random number
+#SET other crew TO number of pirates minus 2
+#SET sent away units TO other crew Mulitiplied by 3
+#SET remaining units TO total units - sent away units
+
+#SET Yondu share TO ROUND remaining units mulitplied 0.13, 2
+#UPDATE remaining units TO remaining units minus Yondu share
+
+#SET Peter share TO ROUND remaining units multiplied 0.11, 2
+#UPDATE remaining units TO remaining units minus Peter share
+
+#SET crew share TO ROUND remaining units divided by number of pirates, 2
+
+#OUTPUT "Units found: " total units
+#OUTPUT "Yondu's share: " Yondu share
+#OUTPUT "Peter's share: " Peter share
+#OUTPUT "Crew's share: " crew share
+#END
